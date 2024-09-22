bd31701584353d5e7960dbdafe63c79290007e37
Rob Levine <rob@roblevine.co.uk>
2024-09-22 18:33:38 +0100

    format main.py

--- main.py
+++ main.py
@@ -12,12 +12,13 @@ from datetime import datetime
 
 SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
 
+
 def get_credentials():
     creds = None
     # Load credentials from file if available
     if os.path.exists("token.json"):
         creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-    
+
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
@@ -28,66 +29,84 @@ def get_credentials():
         # Save the credentials for the next run
         with open("token.json", "w") as token:
             token.write(creds.to_json())
-    
+
     return creds
 
+
 def fetch_labels(service):
-  try:
-    # Call the Gmail API
-    results = service.users().labels().list(userId="me").execute()
-    labels = results.get("labels", [])
+    try:
+        # Call the Gmail API
+        results = service.users().labels().list(userId="me").execute()
+        labels = results.get("labels", [])
 
-    if not labels:
-      logging.info("No labels found.")
-      return []
-    
-    logging.info(f"Labels count: {len(labels)}")
+        if not labels:
+            logging.info("No labels found.")
+            return []
 
-    if logging.getLogger().isEnabledFor(logging.DEBUG):
-      logging.debug("Labels:")
-      for label in labels:
-        logging.debug(f"{label['id']}: {label['name']}")
-    
-    return labels
-  except HttpError as error:
-    print(f"An error occurred: {error}")
-    return []
+        logging.info(f"Labels count: {len(labels)}")
+
+        if logging.getLogger().isEnabledFor(logging.DEBUG):
+            logging.debug("Labels:")
+            for label in labels:
+                logging.debug(f"{label['id']}: {label['name']}")
+
+        return labels
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+        return []
+
 
 def query_emails_by_label(service, user_id, label_id):
     try:
-        response = service.users().messages().list(userId=user_id, labelIds=[label_id]).execute()
-        messages = response.get('messages', [])
-        
-        if not messages:
-            print('No messages found.')
-        else:
-            print('Messages:')
-            for message in messages:
-                msg_id = message['id']
+        response = (
+            service.users()
+            .messages()
+            .list(userId=user_id, labelIds=[label_id])
+            .execute()
+        )
+        messages = response.get("messages", [])
 
-                msg = service.users().messages().get(userId=user_id, id=msg_id).execute()
-                internal_date = msg['internalDate']
-                internal_date_as_string = datetime.fromtimestamp(int(internal_date)/1000).strftime('%Y-%m-%d %H:%M:%S')
+        if not messages:
+            print("No messages found.")
+        else:
+            print("Messages:")
+            for message in messages:
+                msg_id = message["id"]
+
+                msg = (
+                    service.users().messages().get(userId=user_id, id=msg_id).execute()
+                )
+                internal_date = msg["internalDate"]
+                internal_date_as_string = datetime.fromtimestamp(
+                    int(internal_date) / 1000
+                ).strftime("%Y-%m-%d %H:%M:%S")
                 print(f"Message ID: {msg_id}")
                 print(f"Internal Date: {internal_date_as_string}")
                 print(f"Snippet: {msg['snippet']}")
                 print("--")
                 # Print the email body
-                for part in msg['payload']['parts']:
-                    if part['mimeType'] == 'text/plain':
-                        bodyDataBase64 = part['body']['data']
-                        bodyData = base64.urlsafe_b64decode(bodyDataBase64).decode('utf-8')
+                for part in msg["payload"]["parts"]:
+                    if part["mimeType"] == "text/plain":
+                        bodyDataBase64 = part["body"]["data"]
+                        bodyData = base64.urlsafe_b64decode(bodyDataBase64).decode(
+                            "utf-8"
+                        )
                         print(f"Body: {bodyData}")
     except HttpError as error:
         # TODO: Handle errors from Gmail API.
         print(f"An error occurred: {error}")
 
+
 def main():
-    logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+    logging.basicConfig(
+        level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+    )
     creds = get_credentials()
     service = build("gmail", "v1", credentials=creds)
     fetch_labels(service)
-   # query_emails_by_label(service, "me", "Label_15")
+
+
+# query_emails_by_label(service, "me", "Label_15")
 
 if __name__ == "__main__":
     main()
