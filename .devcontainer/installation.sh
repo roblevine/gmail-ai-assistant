c95541aa5c1663a7da8d758b58556af2a7914ab2
Rob Levine <rob@roblevine.co.uk>
2024-11-15 22:47:12 +0000

    fix line endings

--- .devcontainer/installation.sh
+++ .devcontainer/installation.sh
@@ -3,6 +3,7 @@ echo "starting installation script"
 
 echo "update and install apt packages"
 sudo apt update
+sudo apt upgrade -y
 sudo apt install -y vim iputils-ping dos2unix
 
 echo "create virtual environment"
